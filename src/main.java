86e560a7a18eb58292b8275a2b871fb1f9caf3bb
david <hdavid4321@hotmail.fr>
2016-12-03 13:08:27 +0100

    commit

--- src/main.java
+++ src/main.java
@@ -1,4 +1,20 @@
 
-public class main {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet implementation class main
+ */
+
+public class main {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    
 }
